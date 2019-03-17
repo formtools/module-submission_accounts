e9c33a35adf3a9c5c09d0e8cbf7c56d0ab62ca07
Ben Keen <ben.keen@gmail.com>
2019-03-17 16:45:47 -0700

    2.0.4

--- smarty_plugins/function.user_pages_dropdown.php
+++ smarty_plugins/function.user_pages_dropdown.php
@@ -17,51 +17,53 @@ use FormTools\Templates;
  */
 function smarty_function_user_pages_dropdown($params, &$smarty)
 {
-    $LANG = Core::$L;
+	$LANG = Core::$L;
 
-    if (!Templates::hasRequiredParams($smarty, $params, array("name_id"))) {
-        return "";
+	if (!Templates::hasRequiredParams($smarty, $params, array("name_id"))) {
+		return "";
     }
-    $selected = (isset($params["selected"])) ? $params["selected"] : "";
-    $onchange = (isset($params["onchange"])) ? $params["onchange"] : "";
+	$selected = (isset($params["selected"])) ? $params["selected"] : "";
+	$onchange = (isset($params["onchange"])) ? $params["onchange"] : "";
 
-    $attributes = array(
-        "id" => $params["name_id"],
-        "name" => $params["name_id"],
-        "onchange" => $onchange
-        );
+	$attributes = array(
+		"id" => $params["name_id"],
+		"name" => $params["name_id"],
+		"onchange" => $onchange
+	);
 
-    $attribute_str = "";
-    while (list($key, $value) = each($attributes)) {
-        if (!empty($value)) {
-            $attribute_str .= " $key=\"$value\"";
-        }
-    }
+	$attribute_str = "";
+	while (list($key, $value) = each($attributes)) {
+		if (!empty($value)) {
+			$attribute_str .= " $key=\"$value\"";
+		}
+	}
 
-    $dropdown_str = "<select {$attribute_str}>
+	$dropdown_str = "<select {$attribute_str}>
                      <option value=\"\">{$LANG["phrase_please_select"]}</option>
                      <option value=\"custom_url\" " . (($selected == "custom_url") ? "selected" : "") . ">{$LANG["phrase_custom_url"]}</option>
                      <option value=\"edit_submission\" " . (($selected == "edit_submission") ? "selected" : "") . ">{$LANG["phrase_edit_submission"]}</option>
                      <option value=\"logout\" " . (($selected == "logout") ? "selected" : "") . ">{$LANG["word_logout"]}</option>";
 
-    // if the Pages module is enabled and has options, display that too
-    if (Modules::checkModuleAvailable("pages")) {
-        $pages = Modules::getModuleInstance("pages");
-        $pages_info = $pages->getPages("all");
-        $pages = $pages_info["results"];
+	// if the Pages module is enabled and has options, display that too
+	if (Modules::checkModuleAvailable("pages")) {
+		$pages = Modules::getModuleInstance("pages");
+		$L = $pages->getLangStrings();
 
-        if (!empty($pages)) {
-            $dropdown_str .= "<optgroup label=\"{$LANG["phrase_pages_module"]}\">\n";
-            foreach ($pages as $page) {
-                $page_id = $page["page_id"];
-                $page_name = $page["page_name"];
-                $dropdown_str .= "<option value=\"page_{$page_id}\" " . (($selected == "page_{$page_id}") ? "selected" : "") . ">$page_name</option>\n";
-            }
-            $dropdown_str .= "</optgroup>\n";
-        }
-    }
-    $dropdown_str .= "</select>";
+		$pages_info = $pages->getPages("all");
+		$pages = $pages_info["results"];
 
-    return $dropdown_str;
+		if (!empty($pages)) {
+			$dropdown_str .= "<optgroup label=\"{$L["phrase_pages_module"]}\">\n";
+			foreach ($pages as $page) {
+				$page_id = $page["page_id"];
+				$page_name = $page["page_name"];
+				$dropdown_str .= "<option value=\"page_{$page_id}\" " . (($selected == "page_{$page_id}") ? "selected" : "") . ">$page_name</option>\n";
+			}
+			$dropdown_str .= "</optgroup>\n";
+		}
+	}
+	$dropdown_str .= "</select>";
+
+	return $dropdown_str;
 }
 
